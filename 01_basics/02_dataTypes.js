2cdfd74514342259b44b38494585460db3a22952
Ashish Mishra <74653766+snoopp21@users.noreply.github.com>
2024-12-12 13:58:23 +0000

    Add 02_dataTypes.js for basic data types example

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert(3+3) //we are using node js, not browser
+let name = "ashish"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+console.log(state);
+console.log(isLoggedIn);
+
+
